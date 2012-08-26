f0af37a272c3ccda9577b9588b12362d048ed566
jane <gemini0707@gmail.com>
2012-08-26 12:34:20 -0700

    update user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,56 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+
   def new
     @user = User.new
   end
   def show
     @user = User.find(params[:id])
+#    @microposts = @user.microposts.paginate(page: params[:page])
+    
   end
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
-  def index
+
+  def edit
+    @user = User.find(params[:id])
   end
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+  def update
+   # @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
 end
