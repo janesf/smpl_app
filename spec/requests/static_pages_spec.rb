d299223c1ceff4c8bf23c3ec60e6dcb7992166f5
jane <gemini0707@gmail.com>
2012-08-28 15:06:10 -0700

    chptr 3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -19,12 +19,10 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path } 
 
-    it "should have the title 'Home'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('h1', :text => 'Sample App')
     end
-
     it "should have the base title" do
       page.should have_selector('title',
                         text: "Ruby on Rails Tutorial Sample App")
