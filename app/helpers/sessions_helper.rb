4f709acfbe5791b720430d20b1413fc3f4549193
jane <gemini0707@gmail.com>
2012-09-06 22:32:04 -0700

    check in latest.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,12 +3,15 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
+  
   def current_user=(user)
     @current_user = user
   end
+  
   def current_user?(user)
     user == current_user
   end
+  
   def redirect_back_or(default)
     redirect_to(session[:return_to] || default)
     session.delete(:return_to)
@@ -17,6 +20,7 @@ module SessionsHelper
   def store_location
     session[:return_to] = request.url
   end
+  
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user
@@ -26,10 +30,10 @@ module SessionsHelper
     self.current_user = nil
     cookies.delete(:remember_token)
   end
+  
   def signed_in?
     !current_user.nil?
   end    
-
 
   def signed_in_user
     unless signed_in?
